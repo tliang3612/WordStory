97049231e87c994ef78956192f80770cffea78e1
tliang3612 <78510338+tliang3612@users.noreply.github.com>
2023-09-15 11:04:30 -0700

    Added path handling

--- cmd/gowordweb/main.go
+++ cmd/gowordweb/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"strings"
+	"text/template"
+
+	go_word "github.com/tliang3612/wordstory"
 )
 
 func main() {
+	//define flags
+	port := flag.Int("port", 3000, "the port to start the word story on web")
 	filename := flag.String("file", "gopher.json", "the json file with the goword story")
+
+	// parse defined flags
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *filename)
 
@@ -17,12 +26,86 @@ func main() {
 		panic(err)
 	}
 
-	d := json.NewDecoder(f)
-	var story gowordstory.Story
-	err = d.Decode(&story)
+	story, err := go_word.DecodeJsonToStruct(f)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	tpl := template.Must(template.New("").Parse(storyTemplate))
+
+	h := go_word.NewHandler(
+		story,
+		go_word.WithTemplate(tpl),
+		go_word.WithPathFunc(pathFunc),
+	)
+
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
+	fmt.Printf("Starting the server on port %d \n", *port)
+	//Create http server with port and handler
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
+
 }
+
+func pathFunc(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+
+	if path == "/story" || path == "/story/" {
+		path = "/stories/"
+	}
+
+	//trim "/story/" from returned path
+	return path[len("/story/"):]
+}
+
+var storyTemplate = `
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>{{.Title}}}</title>	
+	</head>
+
+	<body>
+		<div id="mid">
+			<h1>{{ .Title }}</h1>	
+				{{range .Paragraphs}}
+					<p>{{.}}</p>
+				{{end}}
+			<ul>
+				{{range .Options}}
+					<span id="choice"></span>
+					<li href= "/story/{{.Chapter }}">{{.Text }}</li>
+				{{ end }}
+			</ul>
+		</div>
+
+		<style>
+			body {
+				background-color: #DAC0A3;
+			}
+
+			#mid {
+				max-width: 600px;
+				margin: auto;
+				left: 30px;
+				padding: 20px;
+				border: 5px solid #0F2C59;
+				background-color: #EADBC8;
+			}
+
+			h1 {
+				text-align: center;
+			}
+
+			a {
+				padding: 10px;
+				text-decoration: none;
+			}
+
+			#choice {
+				font-size: 48px;
+			}
+		</style>
+	</body>
+</html>`
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
-module go_word
+module github.com/tliang3612/wordstory
 
 go 1.21.1
+
+require (
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
+)
